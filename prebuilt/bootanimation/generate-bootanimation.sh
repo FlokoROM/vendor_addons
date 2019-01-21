8d31bf2ff4d8700eb051a7ee1dfa4160a40bf5a0
lindwurm <lindwurm.q@gmail.com>
2019-01-21 19:01:07 +0900

    floko: Generate our bootanimation
    * wip
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- prebuilt/bootanimation/generate-bootanimation.sh
+++ prebuilt/bootanimation/generate-bootanimation.sh
@@ -5,50 +5,41 @@ HEIGHT="$2"
 HALF_RES="$3"
 OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
 
+if [ -z "$WIDTH" ]; then
+    echo "Warning: bootanimation width not specified"
+    WIDTH="1080"
+fi
+
+if [ -z "$HEIGHT" ]; then
+    echo "Warning: bootanimation height not specified"
+    HEIGHT="1080"
+fi
+
 if [ "$HEIGHT" -lt "$WIDTH" ]; then
-    IMAGEWIDTH="$HEIGHT"
-    IMAGEHEIGHT="$WIDTH"
+    SIZE="$HEIGHT"
 else
-    IMAGEWIDTH="$WIDTH"
-    IMAGEHEIGHT="$HEIGHT"
+    SIZE="$WIDTH"
 fi
 
-IMAGESCALEWIDTH="$IMAGEWIDTH"
-IMAGESCALEHEIGHT=$(expr $IMAGESCALEWIDTH \* 16 / 9)
-BOOTFPS=40
-
-if [ "$HALF_RES" = "true" ]; then
-    IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
-    BOOTFPS=30
+if [ "$HALF_RES" != "false" ]; then
+    IMAGESIZE=$(expr $SIZE / 2)
+else
+    IMAGESIZE="$SIZE"
 fi
 
-IMAGEHEIGHT=$(expr $IMAGEWIDTH \* 16 / 9)
-RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
+RESOLUTION=""$IMAGESIZE"x"$IMAGESIZE""
 
-rm -rf $OUT >> null
-
-for part_cnt in 0 1 2
+for part_cnt in 1 2 3
 do
-    mkdir -p $OUT/bootanimation/part$part_cnt
+    mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
 done
-
-if [ "$IMAGESCALEWIDTH" -ge 1440 ]; then
-    tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation_1440.tar" -C "$OUT/bootanimation/"
-elif [ "$IMAGESCALEWIDTH" -ge 1080 ]; then
-    tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation_1080.tar" -C "$OUT/bootanimation/"
-else
-    tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation_720.tar" -C "$OUT/bootanimation/"
-fi
-
-#mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
+tar xfp "vendor/addons/prebuilt/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' -colors 250 \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
 
 # Create desc.txt
-touch $OUT/bootanimation/desc.txt
-echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT $BOOTFPS" > "$OUT/bootanimation/desc.txt"
+echo "$SIZE $SIZE" 18 > "$OUT/bootanimation/desc.txt"
 cat "vendor/addons/prebuilt/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
 
 # Create bootanimation.zip
 cd "$OUT/bootanimation"
 
 zip -qr0 "$OUT/bootanimation.zip" .
-
