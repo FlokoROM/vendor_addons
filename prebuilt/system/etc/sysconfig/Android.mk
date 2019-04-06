b5cc92c4598e05b1368bb94d9d1811aafb8a0fa1
lindwurm <lindwurm.q@gmail.com>
2019-04-06 20:04:07 +0900

    addons: Remove unused prebuilt packages
    * AdAway
    * crDroidMusic
    * Turbo
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- prebuilt/system/etc/sysconfig/Android.mk
+++ prebuilt/system/etc/sysconfig/Android.mk
@@ -15,13 +15,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := turbo.xml
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := dialer_experience.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
