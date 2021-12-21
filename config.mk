90be673323748c615904d69edc4d9f3b5f2722d7
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    addons: Bring back UDFPS icons and animations [3/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- config.mk
+++ config.mk
@@ -17,6 +17,11 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 DEVICE_PACKAGE_OVERLAYS += vendor/addons/overlay/common
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/addons/overlay/common
 
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
+    UdfpsResources
+endif
+
 # Turbo Prebuilt package for Battery Estimates
 PRODUCT_PACKAGES += \
     TurboPrebuilt
